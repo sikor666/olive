676a24e6f594e4d766f82baba465d60f87d4e404
Tomek <sikor666@gmail.com>
2018-12-27 01:34:37 +0100

    Change stun server address

--- src/network/Config.hpp
+++ src/network/Config.hpp
@@ -2,7 +2,7 @@
 
 constexpr auto BindPort = "7043";
 
-constexpr auto StunAddr = "216.93.246.18";
+constexpr auto StunAddr = "87.239.90.119"; //"216.93.246.18";
 constexpr auto StunPort = "3478";
 
 constexpr auto ServAddr = "87.239.90.119";
